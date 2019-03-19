8a64f14881d8d1a893aa4e212a441d8ccf86e940
youjie <youjie@datastory.com.cn>
2019-03-19 14:38:54 +0800

    增加meta

--- meta.js
+++ meta.js
@@ -1,6 +1,21 @@
 module.exports={
     prompts:{
-        "lint": {
+        name: {
+            type: 'string',
+            required: true,
+            message: 'Project name',
+        },
+        author: {
+            type: 'string',
+            message: 'Author',
+        },
+        description: {
+            type: 'string',
+            required: false,
+            message: 'Project description',
+            default: '构建一个lib',
+        },
+        lint: {
             "type": "confirm",
             "message": "是否用tslint"
         },
